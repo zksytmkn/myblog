248406accf3df17c371a147b1dfe45c404abefa9
nakamoto <zksytmkn4869@gmail.com>
2023-09-19 00:53:16 +0900

    feat:Implement pagination for the tag list page(#25)

--- app/tags/[tagId]/p/[current]/page.tsx
+++ app/tags/[tagId]/p/[current]/page.tsx
@@ -1,21 +1,23 @@
 import { LIMIT } from '@/constants'
-import { getList, getTag } from '@/libs/microcms'
+import { getList } from '@/libs/microcms'
 import ListItem from '@/components/listItem'
 import Pagination from '@/components/pagination'
 
 type Props = {
   params: {
     tagId: string
+    current: string
   }
 }
 
 export default async function Page({ params }: Props) {
   const { tagId } = params
+  const current = parseInt(params.current as string, 10)
   const data = await getList({
     limit: LIMIT,
+    offset: LIMIT * (current - 1),
     filters: `tags[contains]${tagId}`,
   })
-  const tag = await getTag(tagId)
   return (
     <div>
       <ul>
@@ -23,7 +25,11 @@ export default async function Page({ params }: Props) {
           <ListItem key={article.id} article={article} />
         ))}
       </ul>
-      <Pagination totalCount={data.totalCount} basePath={`/tags/${tagId}`} />
+      <Pagination
+        totalCount={data.totalCount}
+        current={current}
+        basePath={`/tags/${tagId}`}
+      />
     </div>
   )
 }
