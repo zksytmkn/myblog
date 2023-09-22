29252f4bbb3660cf8b56ec6ae11b7eeebc7b7a2d
nakamoto <zksytmkn4869@gmail.com>
2023-09-22 16:38:39 +0900

    refactor:Componentizing the Header and Footer(#37)

---
+++ components/header/index.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Header() {
+  return (
+    <header className="pt-4 px-6 pb-2">
+      <Link href="/">
+        <Image
+          src="/logo.svg"
+          alt="SIMPLE"
+          className="h-6 w-auto"
+          width={348}
+          height={133}
+          priority
+        />
+      </Link>
+    </header>
+  )
+}
